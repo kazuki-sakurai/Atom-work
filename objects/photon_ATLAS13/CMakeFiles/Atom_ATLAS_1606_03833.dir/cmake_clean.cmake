a60f490d0a8629c8081ca3f2d566ce6f210c73fd
spliew <liew@hep-th.phys.s.u-tokyo.ac.jp>
2016-08-01 13:40:53 +0900

    complete analysis

---
+++ objects/photon_ATLAS13/CMakeFiles/Atom_ATLAS_1606_03833.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Atom_ATLAS_1606_03833.dir/ATLAS_1606_03833.cc.o"
+  "libAtom_ATLAS_1606_03833.pdb"
+  "libAtom_ATLAS_1606_03833.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Atom_ATLAS_1606_03833.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
