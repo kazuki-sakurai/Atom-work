245d2beef076a1dbc86b734617c4a7b6c68e5641
kazuki-sakurai <kazuki.sakurai.1225@gmail.com>
2016-08-12 14:22:13 +0100

    tab size 2

--- analyses/ATLAS_CONF_2016_078/ATLAS_CONF_2016_078.cc
+++ analyses/ATLAS_CONF_2016_078/ATLAS_CONF_2016_078.cc
@@ -30,64 +30,64 @@ namespace Atom {
     /// Book histograms and initialise projections before the run
     void init() {
 
-    useDetector( "ATLAS_CMS_all" ); // "ATLAS2016" );
+      useDetector( "ATLAS_CMS_all" ); // "ATLAS2016" );
 
-    FinalState fsbase( getRange( "Full_Range_ATLAS" ) );
+      FinalState fsbase( getRange( "Full_Range_ATLAS" ) );
 
-    Range muDetRange = getRange( "Muon_Range_Detector_ATLAS" );
-    Range hadRange   = getRange( "HCal_Range_ATLAS" );
-    FastJets jets(fsbase, 
-            hadRange & Range( PT > 20 & abseta < 2.8 ), 
-            muDetRange, FastJets::ANTIKT, 0.4 );
-    jets.setSmearingParams( getJetSim( "Jet_Smear_Topo_ATLAS" ) );
-    jets.setEfficiencyParams( getJetEff( "Jet_Ident_PlaceHolder" ) );
+      Range muDetRange = getRange( "Muon_Range_Detector_ATLAS" );
+      Range hadRange   = getRange( "HCal_Range_ATLAS" );
+      FastJets jets(fsbase, 
+              hadRange & Range( PT > 20 & abseta < 2.8 ), 
+              muDetRange, FastJets::ANTIKT, 0.4 );
+      jets.setSmearingParams( getJetSim( "Jet_Smear_Topo_ATLAS" ) );
+      jets.setEfficiencyParams( getJetEff( "Jet_Ident_PlaceHolder" ) );
 
-    HeavyFlavorJets bjets(jets, Range(PT > 50. & abseta < 2.5));
-    bjets.setTaggingEfficiency( *getBJetEff("BJet_Ident_MV1_ATLAS") );
-    bjets.setCurrentWorkingPoint( 0.77 );
-    addProjection(bjets, "BJets");
+      HeavyFlavorJets bjets(jets, Range(PT > 50. & abseta < 2.5));
+      bjets.setTaggingEfficiency( *getBJetEff("BJet_Ident_MV1_ATLAS") );
+      bjets.setCurrentWorkingPoint( 0.77 );
+      addProjection(bjets, "BJets");
 
-    IsoMuon mu_base(Range(PT > 10. & abseta < 2.5));
-    mu_base.addIso(TRACK_ISO_PT, 0.3,  0.1,  0.0, 0.01, CALO_ALL);
-    mu_base.setSmearingParams  ( getMuonSim( "Muon_Smear_ID-MS_ATLAS" ) );
-    mu_base.setEfficiencyParams( getMuonEff( "Muon_Ident_CB-ST_ATLAS" ) );
+      IsoMuon mu_base(Range(PT > 10. & abseta < 2.5));
+      mu_base.addIso(TRACK_ISO_PT, 0.3,  0.1,  0.0, 0.01, CALO_ALL);
+      mu_base.setSmearingParams  ( getMuonSim( "Muon_Smear_ID-MS_ATLAS" ) );
+      mu_base.setEfficiencyParams( getMuonEff( "Muon_Ident_CB-ST_ATLAS" ) );
 
-    
-    IsoElectron ele_base( Range(PT > 10. & abseta < 2.47) );
-    ele_base.addIso(TRACK_ISO_PT, 0.3,  0.1,  0.0, 0.01, CALO_ALL);
-    ele_base.setSmearingParams  ( getElectronSim( "Electron_Smear_run1_ATLAS" ) );
-    ele_base.setEfficiencyParams( getElectronEff( "Electron_Ident_Medium_2012_ATLAS" ) );
+      
+      IsoElectron ele_base( Range(PT > 10. & abseta < 2.47) );
+      ele_base.addIso(TRACK_ISO_PT, 0.3,  0.1,  0.0, 0.01, CALO_ALL);
+      ele_base.setSmearingParams  ( getElectronSim( "Electron_Smear_run1_ATLAS" ) );
+      ele_base.setEfficiencyParams( getElectronEff( "Electron_Ident_Medium_2012_ATLAS" ) );
 
-    // Overlap removal
-    NearIsoParticle jets_clean(jets);
-    jets_clean.addFilter(ele_base, 0.2);
-    addProjection(jets_clean, "Jets");
+      // Overlap removal
+      NearIsoParticle jets_clean(jets);
+      jets_clean.addFilter(ele_base, 0.2);
+      addProjection(jets_clean, "Jets");
 
-    MergedFinalState base_leptons(ele_base, mu_base);
-    NearIsoParticle leptons_clean(base_leptons);
-    leptons_clean.addFilter(jets_clean, 0.4);
-    addProjection(leptons_clean, "Leptons");
+      MergedFinalState base_leptons(ele_base, mu_base);
+      NearIsoParticle leptons_clean(base_leptons);
+      leptons_clean.addFilter(jets_clean, 0.4);
+      addProjection(leptons_clean, "Leptons");
 
-    MergedFinalState met_seed(jets_clean, base_leptons);
-    MissingMomentum met( fsbase, met_seed );
-    met.setSmearingParams( getMETSim( "MissingET_Smear_ETOnly_Grid_PlaceHolder" ) );
-    addProjection(met, "MissingEt");
+      MergedFinalState met_seed(jets_clean, base_leptons);
+      MissingMomentum met( fsbase, met_seed );
+      met.setSmearingParams( getMETSim( "MissingET_Smear_ETOnly_Grid_PlaceHolder" ) );
+      addProjection(met, "MissingEt");
 
-    Sphericity sphr(jets_clean);
-    addProjection(sphr, "Sphericity");
+      Sphericity sphr(jets_clean);
+      addProjection(sphr, "Sphericity");
 
-    // Projection booking section -- do not edit/remove this comment
-    /// @todo define projections (see examples and manual)
+      // Projection booking section -- do not edit/remove this comment
+      /// @todo define projections (see examples and manual)
       // FastJets jets(fsbase, hadRange & jet_range, muDetRange, FastJets::ANTIKT, 0.6 );
       // addProjection(jets, "Jets");
 
       // Histogram booking section -- do not edit/remove this comment
-    /// @todo book histograms
-    // triplets of numbers correspond to HepData notation d??-x??-y??
-    // bookHisto1D(1,1,1, "Meff");
+      /// @todo book histograms
+      // triplets of numbers correspond to HepData notation d??-x??-y??
+      // bookHisto1D(1,1,1, "Meff");
 
       // Efficiency booking section -- do not edit/remove this comment
-    /// @todo book the efficiencies
+      /// @todo book the efficiencies
       bookEfficiency("2j-800");
       bookEfficiency("2j-1200");
       bookEfficiency("2j-1600");
@@ -102,90 +102,90 @@ namespace Atom {
       bookEfficiency("6j-1800");
       bookEfficiency("6j-2200");
 
-    // bookEfficiency("SR1");
-    // bookEfficiency("SR2","Description of signal region 2 efficiency");
-    // bookEfficiency("CR1","Control region 1", true);
+      // bookEfficiency("SR1");
+      // bookEfficiency("SR2","Description of signal region 2 efficiency");
+      // bookEfficiency("CR1","Control region 1", true);
 
       // Cuts booking section -- do not edit/remove this comment
-    /// @todo book the cuts
-    bookCut("lepton veto: base");
-    bookCut("MET > 250: base");
-    bookCut("pTj1 > 200: base");
-    bookCut("pTj2 > 200: 2j-800");
-    bookCut("|eta(j1,2)| < 0.8: 2j-800");
-    bookCut("dPhiMin_123 > 0.8: 2j-800");
-    bookCut("dPhiMin(i>3) > 0.4: 2j-800");
-    bookCut("MET/sqrt(HT) > 14: 2j-800");
-    bookCut("meff_inc > 800: 2j-800");
-    bookCut("pTj2 > 250: 2j-(1200, 2000)");
-    bookCut("|eta(j1,2)| < 1.2: 2j-(1200, 2000)");
-    bookCut("dPhiMin_123 > 0.8: 2j-(1200, 2000)");
-    bookCut("dPhiMin(i>3) > 0.4: 2j-(1200, 2000)");
-    bookCut("MET/sqrt(HT) > 16: 2j-1200");
-    bookCut("meff_inc > 1200: 2j-1200");
-    bookCut("MET/sqrt(HT) > 18: 2j-1600");
-    bookCut("meff_inc > 1600: 2j-1600");
-    bookCut("MET/sqrt(HT) > 20: 2j-2000");
-    bookCut("meff_inc > 2000: 2j-2000");
-    bookCut("pTj1 > 600: 3j-1200");
-    bookCut("pTj3 > 50: 3j-1200");
-    bookCut("dPhiMin_123 > 0.4: 3j-1200");
-    bookCut("dPhiMin(i>3) > 0.2: 3j-1200");
-    bookCut("MET/sqrt(HT) > 16: 3j-1200");
-    bookCut("meff_inc > 1200: 3j-1200");
-    bookCut("pTj4 > 100: 4j-1000");
-    bookCut("|eta(j1,2)| < 1.2: 4j-1000");
-    bookCut("dPhiMin_123 > 0.4: 4j-1000");
-    bookCut("dPhiMin(i>3) > 0.4: 4j-1000");
-    bookCut("aplanarity > 0.04: 4j-1000");
-    bookCut("MET/meffNj > 0.25: 4j-1000");
-    bookCut("meff_inc > 1000: 4j-1000");
-    bookCut("pTj4 > 100: 4j-1400");
-    bookCut("|eta(j1,2)| < 2.0: 4j-1400");
-    bookCut("dPhiMin_123 > 0.4: 4j-1400");
-    bookCut("dPhiMin(i>3) > 0.4: 4j-1400");
-    bookCut("aplanarity > 0.04: 4j-1400");
-    bookCut("MET/meffNj > 0.25: 4j-1400");
-    bookCut("meff_inc > 1400: 4j-1400");
-    bookCut("pTj4 > 100: 4j-1800");
-    bookCut("|eta(j1,2)| < 2.0: 4j-1800");
-    bookCut("dPhiMin_123 > 0.4: 4j-1800");
-    bookCut("dPhiMin(i>3) > 0.4: 4j-1800");
-    bookCut("aplanarity > 0.04: 4j-1800");
-    bookCut("MET/meffNj > 0.2: 4j-1800");
-    bookCut("meff_inc > 1800: 4j-1800");
-    bookCut("pTj4 > 150: 4j-2200");
-    bookCut("|eta(j1,2)| < 2.0: 4j-2200");
-    bookCut("dPhiMin_123 > 0.4: 4j-2200");
-    bookCut("dPhiMin(i>3) > 0.4: 4j-2200");
-    bookCut("aplanarity > 0.04: 4j-2200");
-    bookCut("MET/meffNj > 0.2: 4j-2200");
-    bookCut("meff_inc > 2200: 4j-2200");
-    bookCut("pTj4 > 150: 4j-2600");
-    bookCut("dPhiMin_123 > 0.4: 4j-2600");
-    bookCut("dPhiMin(i>3) > 0.4: 4j-2600");
-    bookCut("aplanarity > 0.04: 4j-2600");
-    bookCut("MET/meffNj > 0.2: 4j-2600");
-    bookCut("meff_inc > 2600: 4j-2600");
-    bookCut("pTj1 > 500: 5j-1400");
-    bookCut("pTj5 > 50: 5j-1400");
-    bookCut("dPhiMin_123 > 0.4: 5j-1400");
-    bookCut("dPhiMin(i>3) > 0.2: 5j-1400");
-    bookCut("MET/meffNj > 0.3: 5j-1400");
-    bookCut("meff_inc > 1400: 5j-1400");
-    bookCut("pTj6 > 50: 6j-1800");
-    bookCut("|eta(j1,6)| < 2.0: 6j-1800");
-    bookCut("dPhiMin_123 > 0.4: 6j-1800");
-    bookCut("dPhiMin(i>3) > 0.2: 6j-1800");
-    bookCut("aplanarity > 0.08: 6j-1800");
-    bookCut("MET/meffNj > 0.2: 6j-1800");
-    bookCut("meff_inc > 1800: 6j-1800");
-    bookCut("pTj6 > 100: 6j-2200");
-    bookCut("dPhiMin_123 > 0.4: 6j-2200");
-    bookCut("dPhiMin(i>3) > 0.2: 6j-2200");
-    bookCut("aplanarity > 0.08: 6j-2200");
-    bookCut("MET/meffNj > 0.15: 6j-2200");
-    bookCut("meff_inc > 2200: 6j-2200");
+      /// @todo book the cuts
+      bookCut("lepton veto: base");
+      bookCut("MET > 250: base");
+      bookCut("pTj1 > 200: base");
+      bookCut("pTj2 > 200: 2j-800");
+      bookCut("|eta(j1,2)| < 0.8: 2j-800");
+      bookCut("dPhiMin_123 > 0.8: 2j-800");
+      bookCut("dPhiMin(i>3) > 0.4: 2j-800");
+      bookCut("MET/sqrt(HT) > 14: 2j-800");
+      bookCut("meff_inc > 800: 2j-800");
+      bookCut("pTj2 > 250: 2j-(1200, 2000)");
+      bookCut("|eta(j1,2)| < 1.2: 2j-(1200, 2000)");
+      bookCut("dPhiMin_123 > 0.8: 2j-(1200, 2000)");
+      bookCut("dPhiMin(i>3) > 0.4: 2j-(1200, 2000)");
+      bookCut("MET/sqrt(HT) > 16: 2j-1200");
+      bookCut("meff_inc > 1200: 2j-1200");
+      bookCut("MET/sqrt(HT) > 18: 2j-1600");
+      bookCut("meff_inc > 1600: 2j-1600");
+      bookCut("MET/sqrt(HT) > 20: 2j-2000");
+      bookCut("meff_inc > 2000: 2j-2000");
+      bookCut("pTj1 > 600: 3j-1200");
+      bookCut("pTj3 > 50: 3j-1200");
+      bookCut("dPhiMin_123 > 0.4: 3j-1200");
+      bookCut("dPhiMin(i>3) > 0.2: 3j-1200");
+      bookCut("MET/sqrt(HT) > 16: 3j-1200");
+      bookCut("meff_inc > 1200: 3j-1200");
+      bookCut("pTj4 > 100: 4j-1000");
+      bookCut("|eta(j1,2)| < 1.2: 4j-1000");
+      bookCut("dPhiMin_123 > 0.4: 4j-1000");
+      bookCut("dPhiMin(i>3) > 0.4: 4j-1000");
+      bookCut("aplanarity > 0.04: 4j-1000");
+      bookCut("MET/meffNj > 0.25: 4j-1000");
+      bookCut("meff_inc > 1000: 4j-1000");
+      bookCut("pTj4 > 100: 4j-1400");
+      bookCut("|eta(j1,2)| < 2.0: 4j-1400");
+      bookCut("dPhiMin_123 > 0.4: 4j-1400");
+      bookCut("dPhiMin(i>3) > 0.4: 4j-1400");
+      bookCut("aplanarity > 0.04: 4j-1400");
+      bookCut("MET/meffNj > 0.25: 4j-1400");
+      bookCut("meff_inc > 1400: 4j-1400");
+      bookCut("pTj4 > 100: 4j-1800");
+      bookCut("|eta(j1,2)| < 2.0: 4j-1800");
+      bookCut("dPhiMin_123 > 0.4: 4j-1800");
+      bookCut("dPhiMin(i>3) > 0.4: 4j-1800");
+      bookCut("aplanarity > 0.04: 4j-1800");
+      bookCut("MET/meffNj > 0.2: 4j-1800");
+      bookCut("meff_inc > 1800: 4j-1800");
+      bookCut("pTj4 > 150: 4j-2200");
+      bookCut("|eta(j1,2)| < 2.0: 4j-2200");
+      bookCut("dPhiMin_123 > 0.4: 4j-2200");
+      bookCut("dPhiMin(i>3) > 0.4: 4j-2200");
+      bookCut("aplanarity > 0.04: 4j-2200");
+      bookCut("MET/meffNj > 0.2: 4j-2200");
+      bookCut("meff_inc > 2200: 4j-2200");
+      bookCut("pTj4 > 150: 4j-2600");
+      bookCut("dPhiMin_123 > 0.4: 4j-2600");
+      bookCut("dPhiMin(i>3) > 0.4: 4j-2600");
+      bookCut("aplanarity > 0.04: 4j-2600");
+      bookCut("MET/meffNj > 0.2: 4j-2600");
+      bookCut("meff_inc > 2600: 4j-2600");
+      bookCut("pTj1 > 500: 5j-1400");
+      bookCut("pTj5 > 50: 5j-1400");
+      bookCut("dPhiMin_123 > 0.4: 5j-1400");
+      bookCut("dPhiMin(i>3) > 0.2: 5j-1400");
+      bookCut("MET/meffNj > 0.3: 5j-1400");
+      bookCut("meff_inc > 1400: 5j-1400");
+      bookCut("pTj6 > 50: 6j-1800");
+      bookCut("|eta(j1,6)| < 2.0: 6j-1800");
+      bookCut("dPhiMin_123 > 0.4: 6j-1800");
+      bookCut("dPhiMin(i>3) > 0.2: 6j-1800");
+      bookCut("aplanarity > 0.08: 6j-1800");
+      bookCut("MET/meffNj > 0.2: 6j-1800");
+      bookCut("meff_inc > 1800: 6j-1800");
+      bookCut("pTj6 > 100: 6j-2200");
+      bookCut("dPhiMin_123 > 0.4: 6j-2200");
+      bookCut("dPhiMin(i>3) > 0.2: 6j-2200");
+      bookCut("aplanarity > 0.08: 6j-2200");
+      bookCut("MET/meffNj > 0.15: 6j-2200");
+      bookCut("meff_inc > 2200: 6j-2200");
       // bookCut("CutNJets");
       // bookCut("CutPTJ1","description goes here");
       // bookCut("CutEtaJet","this is a control region cut", true);
