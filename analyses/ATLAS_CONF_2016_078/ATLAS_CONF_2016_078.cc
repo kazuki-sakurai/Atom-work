b81a5dda4a49e6ffa9a9b0ee821d9262d03aeebe
kazuki-sakurai <kazuki.sakurai.1225@gmail.com>
2016-08-13 11:12:39 +0100

    histogram 078

--- analyses/ATLAS_CONF_2016_078/ATLAS_CONF_2016_078.cc
+++ analyses/ATLAS_CONF_2016_078/ATLAS_CONF_2016_078.cc
@@ -48,15 +48,15 @@ namespace Atom {
       addProjection(bjets, "BJets");
 
       IsoMuon mu_base(Range(PT > 10. & abseta < 2.5));
-      mu_base.addIso(TRACK_ISO_PT, 0.3,  0.1,  0.0, 0.01, CALO_ALL);
+      mu_base.addIso(TRACK_ISO_PT, 0.2,  0.18,  0.0, 0.01, CALO_ALL);
       mu_base.setSmearingParams  ( getMuonSim( "Muon_Smear_ID-MS_ATLAS" ) );
       mu_base.setEfficiencyParams( getMuonEff( "Muon_Ident_CB-ST_ATLAS" ) );
 
       
       IsoElectron ele_base( Range(PT > 10. & abseta < 2.47) );
-      ele_base.addIso(TRACK_ISO_PT, 0.3,  0.1,  0.0, 0.01, CALO_ALL);
+      ele_base.addIso(TRACK_ISO_PT, 0.2,  0.18,  0.0, 0.01, CALO_ALL);
       ele_base.setSmearingParams  ( getElectronSim( "Electron_Smear_run1_ATLAS" ) );
-      ele_base.setEfficiencyParams( getElectronEff( "Electron_Ident_Medium_2012_ATLAS" ) );
+      ele_base.setEfficiencyParams( getElectronEff( "Electron_Ident_Loose_2012_ATLAS" ) );
 
       // Overlap removal
       NearIsoParticle jets_clean(jets);
@@ -73,7 +73,9 @@ namespace Atom {
       met.setSmearingParams( getMETSim( "MissingET_Smear_ETOnly_Grid_PlaceHolder" ) );
       addProjection(met, "MissingEt");
 
-      Sphericity sphr(jets_clean);
+      Sphericity sphr(fsbase);
+      //Sphericity sphr(jets_clean);
+      //Sphericity sphr;      
       addProjection(sphr, "Sphericity");
 
       // Projection booking section -- do not edit/remove this comment
@@ -85,6 +87,11 @@ namespace Atom {
       /// @todo book histograms
       // triplets of numbers correspond to HepData notation d??-x??-y??
       // bookHisto1D(1,1,1, "Meff");
+      bookHisto1D("Meff-2j-800", 14, 800, 3600, "Meff-2j-800", "Meff(inc)", "Events/200GeV");
+      bookHisto1D("Meff-2j-1600", 14, 800, 3600, "Meff-2j-1600", "Meff(inc)", "Events/200GeV");
+      bookHisto1D("Meff-4j-1000", 14, 800, 3600, "Meff-4j-1000", "Meff(inc)", "Events/200GeV");
+      bookHisto1D("Meff-4j-2200", 14, 800, 3600, "Meff-4j-2200", "Meff(inc)", "Events/200GeV");
+      bookHisto1D("Meff-6j-1800", 14, 800, 3600, "Meff-6j-1800", "Meff(inc)", "Events/200GeV");
 
       // Efficiency booking section -- do not edit/remove this comment
       /// @todo book the efficiencies
@@ -199,148 +206,151 @@ namespace Atom {
     void analyze(const Event& event) {
 
       // Projection application section -- do not edit/remove this comment
-    /// @todo apply projections
-    const Particles& jets = applyProjection<NearIsoParticle>(event, "Jets").particlesByPt();
-    const Particles& leps = applyProjection<NearIsoParticle>(event, "Leptons").particlesByPt();
-    const HeavyFlavorJets& bjproj = applyProjection<HeavyFlavorJets>(event, "BJets");
-    const Particles& bjets = bjproj.getTaggedJets(); 
-    const Particles& untagged_jets = bjproj.getUntaggedJets(); 
-    const MissingMomentum& pmet = applyProjection<MissingMomentum>(event, "MissingEt");
-    const FourMomentum met = pmet.missingEt(); // met is four-momentum but pz and E is set zero
-    const Sphericity& spher = applyProjection<Sphericity>(event, "Sphericity");
+      /// @todo apply projections
+      const Particles& jets = applyProjection<NearIsoParticle>(event, "Jets").particlesByPt();
+      const Particles& leps = applyProjection<NearIsoParticle>(event, "Leptons").particlesByPt();
+      const HeavyFlavorJets& bjproj = applyProjection<HeavyFlavorJets>(event, "BJets");
+      const Particles& bjets = bjproj.getTaggedJets(); 
+      const Particles& untagged_jets = bjproj.getUntaggedJets(); 
+      const MissingMomentum& pmet = applyProjection<MissingMomentum>(event, "MissingEt");
+      const FourMomentum met = pmet.missingEt(); // met is four-momentum but pz and E is set zero
+      //const Sphericity& spher = applyProjection<Sphericity>(event, "Sphericity");
 
-    double MET = met.pT();
-    int Njet = jets.size();
-    double aplanarity = spher.aplanarity();
-    //double aplanarity = 0;
-    //cout << aplanarity << endl;
+      double MET = met.pT();
+      int Njet = jets.size();
+      //double aplanarity = spher.aplanarity();
+      double aplanarity = 0;
+      //cout << aplanarity << endl;
 
-    double meff_Nj[6] = {};
-    for(int i = 0; i < min(Njet, 6); i++){
-      meff_Nj[i] = MET;                
-      for(size_t j = 0; j < i + 1; j++){
-        meff_Nj[i] += jets[j].pT();
+      double meff_Nj[6] = {};
+      for(int i = 0; i < min(Njet, 6); i++){
+        meff_Nj[i] = MET;                
+        for(size_t j = 0; j < i + 1; j++){
+          meff_Nj[i] += jets[j].pT();
+        }
+      } 
+
+      double H_T = 0;
+      double meff_inc = MET;
+      for(int i = 0; i < Njet; i++){
+        double pTj = jets[i].pT();
+          H_T += pTj;
+        if(pTj > 50.) meff_inc += pTj;
+      } 
+
+      double dPhiMin_123 = 1000;
+      for(int i=0; i < min(Njet, 3); i++){
+        double dPhi = deltaPhi(jets[i].momentum(), met);
+        if( dPhi < dPhiMin_123 ) dPhiMin_123 = dPhi;
       }
-    } 
 
-    double H_T = 0;
-    double meff_inc = MET;
-    for(int i = 0; i < Njet; i++){
-      double pTj = jets[i].pT();
-        H_T += pTj;
-      if(pTj > 50.) meff_inc += pTj;
-    } 
+      double dPhiMin_4 = -1;
+      if(Njet > 2) dPhiMin_4 = 1000;
+      for(int i=3; i < Njet; i++){
+        double dPhi = deltaPhi(jets[i].momentum(), met);
+        if( dPhi < dPhiMin_4 ) dPhiMin_4 = dPhi;
+      }
 
-    double dPhiMin_123 = 1000;
-    for(int i=0; i < min(Njet, 3); i++){
-      double dPhi = deltaPhi(jets[i].momentum(), met);
-      if( dPhi < dPhiMin_123 ) dPhiMin_123 = dPhi;
-    }
+      double dPhiMin_all = 1000;
+      for(int i=0; i < Njet; i++){
+        double dPhi = deltaPhi(jets[i].momentum(), met);
+        if( dPhi < dPhiMin_all ) dPhiMin_all = dPhi;
+      }            
 
-    double dPhiMin_4 = -1;
-    if(Njet > 2) dPhiMin_4 = 1000;
-    for(int i=3; i < Njet; i++){
-      double dPhi = deltaPhi(jets[i].momentum(), met);
-      if( dPhi < dPhiMin_4 ) dPhiMin_4 = dPhi;
-    }
+      double maxetaj12 = -100;
+      for(int i=0; i < min(Njet, 2); i++){
+        double eta = jets[i].abseta();
+        if( eta > maxetaj12 ) maxetaj12 = eta;
+      }            
 
-    double dPhiMin_all = 1000;
-    for(int i=0; i < Njet; i++){
-      double dPhi = deltaPhi(jets[i].momentum(), met);
-      if( dPhi < dPhiMin_all ) dPhiMin_all = dPhi;
-    }            
+      double maxetaj16 = -100;
+      for(int i=0; i < min(Njet, 6); i++){
+        double eta = jets[i].abseta();
+        if( eta > maxetaj16 ) maxetaj16 = eta;
+      }            
 
-    double maxetaj12 = -100;
-    for(int i=0; i < min(Njet, 2); i++){
-      double eta = jets[i].abseta();
-      if( eta > maxetaj12 ) maxetaj12 = eta;
-    }            
-
-    double maxetaj16 = -100;
-    for(int i=0; i < min(Njet, 6); i++){
-      double eta = jets[i].abseta();
-      if( eta > maxetaj16 ) maxetaj16 = eta;
-    }            
-
-    //#######################################
+      //#######################################
 
 
-    // ********************************************* //
+      // ********************************************* //
 
-    if( !cut( leps.size(), CUT_EQ, 0, "lepton veto: base" ) ) vetoEvent;
-    if( !cut( MET, CUT_GT, 200., "MET > 250: base" ) ) vetoEvent;
-    if( jets.size() < 1 ) vetoEvent;            
-    if( !cut( jets[0].pT(), CUT_GT, 200., "pTj1 > 200: base" ) ) vetoEvent;
+      if( !cut( leps.size(), CUT_EQ, 0, "lepton veto: base" ) ) vetoEvent;
+      if( !cut( MET, CUT_GT, 200., "MET > 250: base" ) ) vetoEvent;
+      if( jets.size() < 1 ) vetoEvent;            
+      if( !cut( jets[0].pT(), CUT_GT, 200., "pTj1 > 200: base" ) ) vetoEvent;
 
-    if( Njet > 1 ){
-      // 2j-800            
-      if( cut( jets[1].pT(), CUT_GT, 200., "pTj2 > 200: 2j-800" ) ){
-        if( cut( maxetaj12, CUT_LT, 0.8, "|eta(j1,2)| < 0.8: 2j-800" ) ){
-          if( cut( dPhiMin_123, CUT_GT, 0.8, "dPhiMin_123 > 0.8: 2j-800" ) ){
-            if( cut( dPhiMin_4, CUT_GT, 0.4, "dPhiMin(i>3) > 0.4: 2j-800" ) ){
-              if( cut( MET/sqrt(H_T), CUT_GT, 14., "MET/sqrt(HT) > 14: 2j-800" ) ){
-                if( cut( meff_inc, CUT_GT, 800., "meff_inc > 800: 2j-800" ) ){
-                  pass("2j-800");
+      if( Njet > 1 ){
+        // 2j-800            
+        if( cut( jets[1].pT(), CUT_GT, 200., "pTj2 > 200: 2j-800" ) ){
+          if( cut( maxetaj12, CUT_LT, 0.8, "|eta(j1,2)| < 0.8: 2j-800" ) ){
+            if( cut( dPhiMin_123, CUT_GT, 0.8, "dPhiMin_123 > 0.8: 2j-800" ) ){
+              if( cut( dPhiMin_4, CUT_GT, 0.4, "dPhiMin(i>3) > 0.4: 2j-800" ) ){
+                if( cut( MET/sqrt(H_T), CUT_GT, 14., "MET/sqrt(HT) > 14: 2j-800" ) ){
+                  fillPlot("Meff-2j-800", meff_inc);                  
+                  if( cut( meff_inc, CUT_GT, 800., "meff_inc > 800: 2j-800" ) ){
+                    pass("2j-800");
+                  }
+                }
+              }
+            }
+          }
+        }
+        // 2j-(1200-2000)
+        if( cut( jets[1].pT(), CUT_GT, 250., "pTj2 > 250: 2j-(1200, 2000)" ) ){
+          if( cut( maxetaj12, CUT_LT, 1.2, "|eta(j1,2)| < 1.2: 2j-(1200, 2000)" ) ){
+            if( cut( dPhiMin_123, CUT_GT, 0.8, "dPhiMin_123 > 0.8: 2j-(1200, 2000)" ) ){
+              if( cut( dPhiMin_4, CUT_GT, 0.4, "dPhiMin(i>3) > 0.4: 2j-(1200, 2000)" ) ){
+                // 2j-1200
+                if( cut( MET/sqrt(H_T), CUT_GT, 16., "MET/sqrt(HT) > 16: 2j-1200" ) ){
+                  if( cut( meff_inc, CUT_GT, 1200., "meff_inc > 1200: 2j-1200" ) ){
+                    pass("2j-1200");
+                  }
+                }
+                // 2j-1600                        
+                if( cut( MET/sqrt(H_T), CUT_GT, 18., "MET/sqrt(HT) > 18: 2j-1600" ) ){
+                  fillPlot("Meff-2j-1600", meff_inc);                                    
+                  if( cut( meff_inc, CUT_GT, 1600., "meff_inc > 1600: 2j-1600" ) ){
+                    pass("2j-1600");
+                  }
+                }
+                // 2j-2000                        
+                if( cut( MET/sqrt(H_T), CUT_GT, 20., "MET/sqrt(HT) > 20: 2j-2000" ) ){
+                  if( cut( meff_inc, CUT_GT, 2000., "meff_inc > 2000: 2j-2000" ) ){
+                    pass("2j-2000");
+                  }
                 }
               }
             }
           }
         }
       }
-      // 2j-(1200-2000)
-      if( cut( jets[1].pT(), CUT_GT, 250., "pTj2 > 250: 2j-(1200, 2000)" ) ){
-        if( cut( maxetaj12, CUT_LT, 1.2, "|eta(j1,2)| < 1.2: 2j-(1200, 2000)" ) ){
-          if( cut( dPhiMin_123, CUT_GT, 0.8, "dPhiMin_123 > 0.8: 2j-(1200, 2000)" ) ){
-            if( cut( dPhiMin_4, CUT_GT, 0.4, "dPhiMin(i>3) > 0.4: 2j-(1200, 2000)" ) ){
-              // 2j-1200
-              if( cut( MET/sqrt(H_T), CUT_GT, 16., "MET/sqrt(HT) > 16: 2j-1200" ) ){
-                if( cut( meff_inc, CUT_GT, 1200., "meff_inc > 1200: 2j-1200" ) ){
-                  pass("2j-1200");
-                }
-              }
-              // 2j-1600                        
-              if( cut( MET/sqrt(H_T), CUT_GT, 18., "MET/sqrt(HT) > 18: 2j-1600" ) ){
-                if( cut( meff_inc, CUT_GT, 1600., "meff_inc > 1600: 2j-1600" ) ){
-                  pass("2j-1600");
-                }
-              }
-              // 2j-2000                        
-              if( cut( MET/sqrt(H_T), CUT_GT, 20., "MET/sqrt(HT) > 20: 2j-2000" ) ){
-                if( cut( meff_inc, CUT_GT, 2000., "meff_inc > 2000: 2j-2000" ) ){
-                  pass("2j-2000");
+
+      // 3j-1200            
+      if( cut( jets[0].pT(), CUT_GT, 600., "pTj1 > 600: 3j-1200" ) ){
+        if( Njet > 2 ){
+          if( cut( jets[2].pT(), CUT_GT, 50., "pTj3 > 50: 3j-1200" ) ){
+            if( cut( dPhiMin_123, CUT_GT, 0.4, "dPhiMin_123 > 0.4: 3j-1200" ) ){
+              if( cut( dPhiMin_4, CUT_GT, 0.2, "dPhiMin(i>3) > 0.2: 3j-1200" ) ){
+                if( cut( MET/sqrt(H_T), CUT_GT, 16., "MET/sqrt(HT) > 16: 3j-1200" ) ){
+                  if( cut( meff_inc, CUT_GT, 1200., "meff_inc > 1200: 3j-1200" ) ){
+                    pass("3j-1200");
+                  }
                 }
               }
             }
           }
         }
       }
-    }
 
-    // 3j-1200            
-    if( cut( jets[0].pT(), CUT_GT, 600., "pTj1 > 600: 3j-1200" ) ){
-      if( Njet > 2 ){
-        if( cut( jets[2].pT(), CUT_GT, 50., "pTj3 > 50: 3j-1200" ) ){
-          if( cut( dPhiMin_123, CUT_GT, 0.4, "dPhiMin_123 > 0.4: 3j-1200" ) ){
-            if( cut( dPhiMin_4, CUT_GT, 0.2, "dPhiMin(i>3) > 0.2: 3j-1200" ) ){
-              if( cut( MET/sqrt(H_T), CUT_GT, 16., "MET/sqrt(HT) > 16: 3j-1200" ) ){
-                if( cut( meff_inc, CUT_GT, 1200., "meff_inc > 1200: 3j-1200" ) ){
-                  pass("3j-1200");
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    if( Njet > 3 ){
-      // 4j-1000
-      if( cut( jets[3].pT(), CUT_GT, 100., "pTj4 > 100: 4j-1000" ) ){
-        if( cut( maxetaj12, CUT_LT, 1.2, "|eta(j1,2)| < 1.2: 4j-1000" ) ){
+      if( Njet > 3 ){
+        // 4j-1000
+        if( cut( jets[3].pT(), CUT_GT, 100., "pTj4 > 100: 4j-1000" ) ){
+          if( cut( maxetaj12, CUT_LT, 1.2, "|eta(j1,2)| < 1.2: 4j-1000" ) ){
             if( cut( dPhiMin_123, CUT_GT, 0.4, "dPhiMin_123 > 0.4: 4j-1000" ) ){
               if( cut( dPhiMin_4, CUT_GT, 0.4, "dPhiMin(i>3) > 0.4: 4j-1000" ) ){
                 if( cut( aplanarity, CUT_GT, 0.04, "aplanarity > 0.04: 4j-1000" ) ){
                   if( cut( MET/meff_Nj[3], CUT_GT, 0.25, "MET/meffNj > 0.25: 4j-1000" ) ){
+                    fillPlot("Meff-4j-1000", meff_inc);
                     if( cut( meff_inc, CUT_GT, 1000., "meff_inc > 1000: 4j-1000" ) ){
                       pass("4j-1000");
                     }
@@ -389,6 +399,7 @@ namespace Atom {
               if( cut( dPhiMin_4, CUT_GT, 0.4, "dPhiMin(i>3) > 0.4: 4j-2200" ) ){
                 if( cut( aplanarity, CUT_GT, 0.04, "aplanarity > 0.04: 4j-2200" ) ){
                   if( cut( MET/meff_Nj[3], CUT_GT, 0.2, "MET/meffNj > 0.2: 4j-2200" ) ){
+                    fillPlot("Meff-4j-2200", meff_inc);                    
                     if( cut( meff_inc, CUT_GT, 2200., "meff_inc > 2200: 4j-2200" ) ){
                       pass("4j-2200");
                     }
@@ -413,6 +424,8 @@ namespace Atom {
           }
         }
       }
+      
+
       // 5j-1400
       if( cut( jets[0].pT(), CUT_GT, 500., "pTj1 > 500: 5j-1400" ) ){
         if(Njet > 4){
@@ -438,6 +451,7 @@ namespace Atom {
             if( cut( dPhiMin_4, CUT_GT, 0.2, "dPhiMin(i>3) > 0.2: 6j-1800" ) ){
               if( cut( aplanarity, CUT_GT, 0.08, "aplanarity > 0.08: 6j-1800" ) ){                      
                 if( cut( MET/meff_Nj[5], CUT_GT, 0.2, "MET/meffNj > 0.2: 6j-1800" ) ){
+                  fillPlot("Meff-6j-1000", meff_inc);                                      
                   if( cut( meff_inc, CUT_GT, 1800., "meff_inc > 1800: 6j-1800" ) ){
                     pass("6j-1800");
                   }
@@ -460,20 +474,21 @@ namespace Atom {
             }
           }
         }
-      }
+      }    
       /// @todo fill histograms 
       // fillPlot("Meff", meff);
-
-      // End analyze section -- do not edit/remove this comment
-    }
-
+    } // End analyze section -- do not edit/remove this comment
 
     /// Normalise histograms etc., after the run
     void finalize() {
       // Histogram normalization section -- do not edit/remove this comment
       /// @todo normalize the histograms
       // scale("Mjj");
-
+      scale("Meff-2j-800");
+      scale("Meff-2j-1600");
+      scale("Meff-4j-1000");
+      scale("Meff-4j-2200");
+      scale("Meff-6j-1800");
       // End finalize section -- do not edit/remove this comment
     }
 
